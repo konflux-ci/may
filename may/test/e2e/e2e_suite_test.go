44448feb132d184053a3e389afca507f36951f4b
Andy Sadler <ansadler@redhat.com>
2026-02-26 02:16:29 -0600

    allow running e2e tests on podman kind clusters (#12)
    * allow running e2e tests on podman kind clusters
    
    Loading images from the docker runtime fails to work if the kind cluster
    isn't using docker as its backend.  Instead, export the image to disk
    and load it as an image archive.
    
    Fixes: KFLUXINFRA-2937
    Signed-off-by: Andy Sadler <ansadler@redhat.com>
    
    * Update may/Makefile
    
    Co-authored-by: Francesco Ilario <filario@redhat.com>
    
    ---------
    
    Signed-off-by: Andy Sadler <ansadler@redhat.com>
    Co-authored-by: Francesco Ilario <filario@redhat.com>

--- may/test/e2e/e2e_suite_test.go
+++ may/test/e2e/e2e_suite_test.go
@@ -57,14 +57,16 @@ func TestE2E(t *testing.T) {
 
 var _ = BeforeSuite(func() {
 	By("building the manager(Operator) image")
-	cmd := exec.Command("make", "docker-build", fmt.Sprintf("IMG=%s", projectImage))
+	imgParam := fmt.Sprintf("IMG=%s", projectImage)
+	cmd := exec.Command("make", "docker-build", imgParam)
 	_, err := utils.Run(cmd)
 	ExpectWithOffset(1, err).NotTo(HaveOccurred(), "Failed to build the manager(Operator) image")
 
 	// TODO(user): If you want to change the e2e test vendor from Kind, ensure the image is
 	// built and available before running the tests. Also, remove the following block.
 	By("loading the manager(Operator) image on Kind")
-	err = utils.LoadImageToKindClusterWithName(projectImage)
+	cmd = exec.Command("make", "load-image", imgParam)
+	_, err = utils.Run(cmd)
 	ExpectWithOffset(1, err).NotTo(HaveOccurred(), "Failed to load the manager(Operator) image into Kind")
 
 	// The tests-e2e are intended to run on a temporary cluster that is created and destroyed for testing.
