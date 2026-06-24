67872a88bbb66761cda50866032f5da507213b65
Francesco Ilario <filario@redhat.com>
2026-06-24 08:12:51 +0200

    add tests for runner initialization
    Signed-off-by: Francesco Ilario <filario@redhat.com>
    
    rh-pre-commit.version: 2.4.0
    rh-pre-commit.check-secrets: ENABLED

--- may/internal/controller/provisioner/runner_controller_test.go
+++ may/internal/controller/provisioner/runner_controller_test.go
@@ -18,75 +18,135 @@ package provisioner
 
 import (
 	"context"
+	"fmt"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/utils/ptr"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	maykonfluxcidevv1alpha1 "github.com/konflux-ci/may/api/v1alpha1"
+	"github.com/konflux-ci/may/pkg/runner"
 )
 
-var _ = Describe("Runner Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+var image = "registry.access.redhat.com/ubi10/ubi-micro@sha256:f86852f349dcd2b9ebccef4c8a46fdb75ff2fef9fde8581cef1feddb706be7ba"
 
-		ctx := context.Background()
+var _ = Describe("Runner Controller", Ordered, Serial, func() {
+	Context("When reconciling a resource", func() {
+		const (
+			runnerName = "test-resource"
+			namespace  = "default"
+		)
+
+		var controllerReconciler *RunnerReconciler
 
 		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
+			Name:      runnerName,
+			Namespace: namespace,
 		}
-		host := &maykonfluxcidevv1alpha1.Runner{}
 
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind Runner")
-			err := k8sClient.Get(ctx, typeNamespacedName, host)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &maykonfluxcidevv1alpha1.Runner{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
-						Namespace: "default",
-					},
-					Spec: maykonfluxcidevv1alpha1.RunnerSpec{
-						Flavor: "my-flavor",
-						Resources: v1.ResourceList{
-							v1.ResourceCPU: resource.MustParse("1"),
-						},
-					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
-		})
-
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &maykonfluxcidevv1alpha1.Runner{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
-
-			By("Cleanup the specific resource instance Runner")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &RunnerReconciler{
+		BeforeAll(func(ctx context.Context) {
+			By("Create RunnerReconciler")
+			controllerReconciler = &RunnerReconciler{
 				Client: k8sClient,
 				Scheme: k8sClient.Scheme(),
 			}
 
+			By("creating a Runner")
+			runner := &maykonfluxcidevv1alpha1.Runner{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      runnerName,
+					Namespace: namespace,
+				},
+				Spec: maykonfluxcidevv1alpha1.RunnerSpec{
+					Queue: &maykonfluxcidevv1alpha1.RunnerQueue{
+						Cohort: "my-cohort",
+					},
+					Flavor: "my-flavor",
+					Resources: v1.ResourceList{
+						v1.ResourceCPU: resource.MustParse("1"),
+					},
+					Hooks: &maykonfluxcidevv1alpha1.RunnerHooks{
+						Provisioning: []maykonfluxcidevv1alpha1.RunnerHookPodTemplateSpec{
+							{
+								Name: "provisioning-pod",
+								Template: v1.PodTemplateSpec{
+									Spec: v1.PodSpec{
+										RestartPolicy: v1.RestartPolicyNever,
+										Containers: []v1.Container{
+											{
+												Name:          "provisioning-container",
+												RestartPolicy: ptr.To(v1.ContainerRestartPolicyNever),
+												Image:         image,
+												Command:       []string{"exit"},
+												Args:          []string{"0"},
+											},
+										},
+									},
+								},
+							},
+						},
+						Cleanup: []maykonfluxcidevv1alpha1.RunnerHookPodTemplateSpec{},
+					},
+				},
+			}
+			Expect(k8sClient.Create(ctx, runner)).To(Succeed())
+		})
+
+		AfterAll(func(ctx context.Context) {
+			// TODO(user): Cleanup logic after each test, like removing the runner instance.
+			runner := &maykonfluxcidevv1alpha1.Runner{}
+			err := k8sClient.Get(ctx, typeNamespacedName, runner)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance Runner")
+			Expect(k8sClient.Delete(ctx, runner)).To(Succeed())
+		})
+
+		It("initializes the runner", func(ctx context.Context) {
+			By("Reconciling the created resource")
 			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
 				NamespacedName: typeNamespacedName,
 			})
 			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
+
+			r := maykonfluxcidevv1alpha1.Runner{}
+			Expect(k8sClient.Get(ctx, typeNamespacedName, &r)).To(Succeed())
+			Expect(r.Finalizers).To(Equal([]string{RunnerControllerFinalizer}))
+			Expect(r.Status).ToNot(BeNil())
+			Expect(runner.IsInitializing(r)).To(BeTrue())
 		})
+
+		It("creates the first provisioning pod", func(ctx context.Context) {
+			By("Reconciling the created resource")
+			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
+				NamespacedName: typeNamespacedName,
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			r := maykonfluxcidevv1alpha1.Runner{}
+			Expect(k8sClient.Get(ctx, typeNamespacedName, &r)).To(Succeed())
+
+			p := corev1.Pod{}
+			pk := types.NamespacedName{
+				Name: fmt.Sprintf("%s-%s-%s",
+					"p",
+					r.Name,
+					r.Spec.Hooks.Provisioning[0].Name),
+				Namespace: r.Namespace,
+			}
+
+			Expect(k8sClient.Get(ctx, pk, &p)).To(Succeed())
+			Expect(p.Spec).ToNot(Equal(r.Spec.Hooks.Provisioning[0].Template))
+			Expect(controllerutil.HasControllerReference(&p)).To(BeTrue())
+		})
+
 	})
 })
